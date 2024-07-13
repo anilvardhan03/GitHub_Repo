7ed8040e864926958209b562befec1b228a2564f
anilvardhan03 <nagamoulianil03@gmail.com>
2024-07-13 19:59:46 +0530

    Dev1 - Arithmetic Operations

--- GitMavenProj01/src/main/java/com/anil/main/ApplicationRun.java
+++ GitMavenProj01/src/main/java/com/anil/main/ApplicationRun.java
@@ -1,5 +1,10 @@
 package com.anil.main;
 
-public class ApplicationRun {
+import com.anil.service.Arithmetic;
 
+public class ApplicationRun {
+	public static void main(String[] args) {
+		Arithmetic ar = new Arithmetic();
+		System.out.println("Sum is :: "+ar.sum(10, 20));
+	}
 }
--- GitMavenProj01/src/test/java/com/anil/AppTest.java
+++ GitMavenProj01/src/test/java/com/anil/AppTest.java
@@ -1,21 +1,24 @@
 package com.anil;
 
-
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
+import com.anil.service.Arithmetic;
+
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
-    }
+public class AppTest {
+	/**
+	 * Rigorous Test :-)
+	 */
+	@Test
+	public void testwithPositives() {
+		Arithmetic ar = new Arithmetic();
+		int expected = 40;
+		int actual = ar.sum(10, 30);
+		assertEquals(expected, actual);
+	}
 }
