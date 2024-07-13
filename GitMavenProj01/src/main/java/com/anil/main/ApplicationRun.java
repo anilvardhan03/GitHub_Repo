3075eea5c4c7a4e223c0cecce67ef56dd7f84cc9
anilvardhan03 <nagamoulianil03@gmail.com>
2024-07-13 20:24:36 +0530

    Tl

--- GitMavenProj01/src/main/java/com/anil/main/ApplicationRun.java
+++ GitMavenProj01/src/main/java/com/anil/main/ApplicationRun.java
@@ -4,10 +4,6 @@ import com.anil.service.Arithmetic;
 
 public class ApplicationRun {
 	public static void main(String[] args) {
-<<<<<<< HEAD
-=======
-		Arithmetic ar = new Arithmetic();
-		System.out.println("Sum is :: "+ar.sum(10, 20));
->>>>>>> branch 'master' of https://github.com/anilvardhan03/GitHub_Repo.git
+
 	}
 }
